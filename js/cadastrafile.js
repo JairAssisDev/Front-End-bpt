941604e2c2f402d23feb0117cf81d8296df1b49a
JairAssisDev <jairv141@gmail.com>
2024-07-01 15:47:09 -0300

    atlz

--- js/cadastrafile.js
+++ js/cadastrafile.js
@@ -1,63 +1,94 @@
 ip = "localhost"
-async function uploadcsv() {
-  const file = document.getElementById('fileCSV').files[0];;
-  const formData = new FormData();
-  formData.append('file', file)
 
-  const response = await fetch('http://localhost:5000/paciente/uploadcsv',
-    {
+async function uploadcsv() {
+  document.getElementById("loadresponsecsv").innerHTML = '<svg viewBox="0 0 100 100"> <path d="M 50 96 a 46 46 0 0 1 0 -92 46 46 0 0 1 0 92" /> </svg>';
+  const file = document.getElementById('fileCSV').files[0];
+  const formData = new FormData();
+  formData.append('file', file);
+
+  try {
+    const response = await fetch('http://localhost:5000/paciente/uploadcsv', {
       method: 'POST',
       body: formData
     });
-  const status = await response.status; 
-  const result = await response.json();
 
-  const resultadoElement = document.getElementById('responsecsv');
-  if (resultadoElement) { 
-    if (status === 200) {
-      resultadoElement.textContent = 'Arquivo enviado com sucesso!';
-    } else {
-      resultadoElement.textContent = `Erro ao enviar o arquivo: ${result.mensage}`;
+    if (!response.ok) {
+      throw new Error(`Erro ao enviar o arquivo: ${response.statusText}`);
     }
-  } else {
-    console.error("Elemento 'resultado' não encontrado no DOM");
+
+    const result = await response.json();
+
+    document.getElementById("loadresponsecsv").innerHTML = '';
+
+    if (response.status === 200) {
+    
+      document.getElementById('responsecsv').innerText = result.message;
+      
+    } else if (response.status === 207) {
+        alert("oi",response.status)
+        document.getElementById('responsecsv').innerText = result.message;
+        const patientData = result.naosalvos.map(patient => JSON.stringify(patient)).join('\n');
+        document.getElementById('responsecsvnaosalvos').innerText = patientData;
+      
+
+    } else {
+      throw new Error(`Erro desconhecido: ${result.message}`);
+    }
+  } catch (error) {
+    console.error('Erro na requisição:', error);
+    const responseElement = document.getElementById('responsecsv');
+    if (responseElement) {
+      responseElement.innerText = `Erro na requisição: ${error.message}`;
+    } else {
+      console.error("Elemento 'responsecsv' não encontrado no DOM");
+    }
   }
 }
+
+
+
 
 async function uploadxlsx() {
-  const file = document.getElementById('filexlsx').files[0];;
+  document.getElementById("loadresponsexlsx").innerHTML = '<svg viewBox="0 0 100 100"> <path d="M 50 96 a 46 46 0 0 1 0 -92 46 46 0 0 1 0 92" /> </svg>';
+  const file = document.getElementById('filexlsx').files[0];
   const formData = new FormData();
-  formData.append('file', file)
+  formData.append('file', file);
 
-  const response = await fetch('http://localhost:5000/paciente/uploadxlsx',
-    {
+  try {
+    const response = await fetch('http://localhost:5000/paciente/uploadxlsx', {
       method: 'POST',
       body: formData
     });
-  const status = await response[1]; 
-  const result = await response.json();
 
-  const resultadoElement = document.getElementById('responsexlsx');
-  if (resultadoElement) { 
-    if (status == 200) {
-      console.log(response[1])
-      console.log(response[0])
-      resultadoElement.textContent = result.naosalvos;
+    if (!response.ok) {
+      throw new Error(`Erro ao enviar o arquivo: ${response.statusText}`);
+    }
 
-    }else if(status == 207){
-      console.log(response[1])
-      console.log(response[0])
-      resultadoElement.textContent = response.naosalvos;
+    const result = await response.json();
+
+    document.getElementById("loadresponsexlsx").innerHTML = '';
+
+    if (response.status === 200) {
+      document.getElementById('responsexlsx').innerText = result.message;
+      if (result.naosalvos && result.naosalvos.length > 0) {
+        const patientData = result.naosalvos.map(patient => JSON.stringify(patient)).join('\n');
+        document.getElementById('responsexlsxnaosalvos').innerText = patientData;
+      } else {
+        document.getElementById('responsexlsxnaosalvos').innerText = 'Nenhum paciente não salvo encontrado.';
+      }
+    } else if (response.status === 207) {
+      document.getElementById('responsexlsx').innerText = result.message;
+      const patientData = result.naosalvos.map(patient => JSON.stringify(patient)).join('\n');
+      document.getElementById('responsexlsxnaosalvos').innerText = patientData;
+    } else {
+      throw new Error(`Erro desconhecido: ${result.message}`);
     }
-     else {
-      console.log(result)
-      console.log(status)
-      resultadoElement.textContent = `Erro ao enviar o arquivo: ${result.mensage}`;
-    }
-  } else {
-    console.error("Elemento 'resultado' não encontrado no DOM");
+  } catch (error) {
+    console.error('Erro na requisição:', error);
+    document.getElementById('responsexlsx').innerText = `Erro na requisição: ${error.message}`;
   }
 }
+
 
 
 function getToken() {
