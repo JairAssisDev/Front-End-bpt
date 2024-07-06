313e348e4292c1e35b47c0ad8b00373d91777e1d
JairAssisDev <63867699+JairAssisDev@users.noreply.github.com>
2024-07-06 16:36:33 -0300

    Update cadastrafile.js

--- js/cadastrafile.js
+++ js/cadastrafile.js
@@ -1,4 +1,4 @@
-ip = "localhost"
+ip = "192.168.0.103"
 
 async function uploadFile() {
   document.getElementById("loadresponse").innerHTML = '<svg viewBox="0 0 100 100"> <path d="M 50 96 a 46 46 0 0 1 0 -92 46 46 0 0 1 0 92" /> </svg>';
